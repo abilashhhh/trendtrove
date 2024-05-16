54f428f40e008956ab8b9bec7838aeef13d95822
Rummyspace <abilashnarayanan2001@gmail.com>
2024-05-16 10:21:11 +0530

    till first review

--- frontend/src/Pages/BasePage.tsx
+++ frontend/src/Pages/BasePage.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import TrendTroveLogo from "../Components/Logo/TrendTroveLogo";
+import { Link, useNavigate } from "react-router-dom";
 
 function BasePage() {
+  const navigate = useNavigate();
+
   return (
-    <div>BasePage</div>
-  )
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+       <div className="text-center text-slate-600 font-bold mt-1 lg:mt-2 text-sm lg:text-base">
+      Welcome to
+      </div>
+      <TrendTroveLogo />
+     <div className="flex justify-center gap-3 pt-5">
+     <div className="text-center">
+        <Link
+          to="/signup"
+          className="mt-6 sm:text-l text-xl font-extrabold text-gray-900 hover:text-blue-900">
+          Create an account
+        </Link>
+      </div>
+      | 
+      <div className="text-center">
+        <Link
+          to="/signin"
+          className="mt-6 sm:text-l text-xl font-extrabold text-gray-900 hover:text-blue-900">
+          Login to your account
+        </Link>
+      </div>
+     </div>
+    </div>
+  );
 }
 
-export default BasePage
+export default BasePage;
--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import TrendTroveLogo from "../Components/Logo/TrendTroveLogo";
 
 function HomePage() {
   return (
-    <div className='bg-white min-h-screen'>
-      <div>HomePage</div>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <TrendTroveLogo />
+      <div className="flex justify-center gap-3 pt-5">
+        <div className="text-center">
+          <h1 className="mt-6 sm:text-l text-xl font-extrabold text-gray-900 hover:text-blue-900">
+            Welcome to your account | HOMEPAGE
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
