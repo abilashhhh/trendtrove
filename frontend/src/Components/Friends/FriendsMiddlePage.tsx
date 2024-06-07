9510aa46a86317c0d410d9fa7b6e651c531b0ccf
Rummyspace <abilashnarayanan2001@gmail.com>
2024-06-07 23:08:07 +0530

    changes

--- frontend/src/Components/Friends/FriendsMiddlePage.tsx
+++ frontend/src/Components/Friends/FriendsMiddlePage.tsx
@@ -1,3 +1,4 @@
+// FriendsMiddlePage.tsx
 import React, { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -62,66 +63,63 @@ const FriendsMiddlePage: React.FC = () => {
       <ToastContainer />
       <main className="flex-1 pt-2 p-2 overflow-auto bg-gray-800 dark:bg-gray-700 text-black dark:text-white">
         <div className="p-2 rounded-lg bg-gray-100 dark:bg-gray-900 text-black dark:text-white h-full ">
-          
           <div className="flex flex-col md:flex-row gap-2 h-full">
-            
-            <div className="w-full sm:w-3/6 md:w-1/6 lg:w-2/6 rounded-lg flex flex-col overflow-y-auto no-scrollbar  text-black dark:text-white text-xl p-2 space-y-4">
-              <div className="rounded-lg p-0.5 flex gap-1 h-12 min-w-full  ">
+            <div className="w-full md:w-1/5 rounded-lg flex flex-col text-black dark:text-white text-xl">
+              <div className="p-2 rounded-lg bg-gray-800 flex items-center">
                 <input
                   type="text"
                   onChange={e => e.target.value}
                   placeholder="Search here.."
-                  className="bg-gray-800 text-white sm:w-4/6 md:w-4/6 lg:w-5/6 w-full rounded-lg   p-2"
+                  className="bg-gray-800 text-white w-full p-2 rounded-lg mr-2"
                 />
-
-                <FaSearch className=" sm:w-/6 md:w-2/6 lg:w-1/6 h-full w-10 bg-gray-800 lg:p-3 sm:p-2 rounded-lg p-1" />
+                <FaSearch className="w-8 h-8 bg-gray-800 p-2 rounded-lg" />
               </div>
-<  div className="w-full rounded-lg flex flex-col overflow-y-auto no-scrollbar  text-black dark:text-white text-xl p-2 space-y-4 ">
-{users.length > 0 ? (
-                users.map(user => (
-                  <div key={user._id} >
-                    <button
-                      onClick={() => setActiveSection(user.username)}
-                      className={`w-full rounded-lg p-4 transition duration-300 ease-in-out shadow-md flex items-center space-x-4 ${
-                        activeSection === user.username
-                          ? "bg-gray-400 dark:bg-slate-600"
-                          : "bg-gray-200 dark:bg-slate-800 hover:bg-gray-300 dark:hover:bg-slate-500"
-                      }`}>
-                      <img
-                        src={user.dp || "/default-profile.jpg"}
-                        className="rounded-full w-12 h-12 object-cover"
-                        alt={`${user.username}'s profile`}
-                      />
-                      <div className="flex-1 flex flex-col text-left">
-                        <h3 className="font-semibold text-lg">
-                          {user.username}
-                        </h3>
-                        <p className="text-sm text-gray-600 dark:text-gray-300">
-                          {user.name}
-                        </p>
-                      </div>
-                    </button>
-                    {activeSection === user.username && (
-                      <div className="block sm:hidden mt-4">
-                        <ProfileSectionFriendsPage
-                          userDetails={user}
-                          currentUser={currentUser}
+              <div className="overflow-y-auto no-scrollbar text-black dark:text-white text-xl p-2 space-y-4 ">
+                {users.length > 0 ? (
+                  users.map(user => (
+                    <div key={user._id}>
+                      <button
+                        onClick={() => setActiveSection(user.username)}
+                        className={`w-full rounded-lg p-4 transition duration-300 ease-in-out shadow-md flex items-center space-x-4 ${
+                          activeSection === user.username
+                            ? "bg-gray-400 dark:bg-slate-600"
+                            : "bg-gray-200 dark:bg-slate-800 hover:bg-gray-300 dark:hover:bg-slate-500"
+                        }`}
+                      >
+                        <img
+                          src={user.dp || "/default-profile.jpg"}
+                          className="rounded-full w-12 h-12 object-cover"
+                          alt={`${user.username}'s profile`}
                         />
-                      </div>
-                    )}
-                  </div>
-                ))
-              ) : (
-                <p className="text-center text-gray-600 dark:text-gray-300 no-scrollbar">
-                  No users found
-                </p>
-              )}
-</div>
-             
+                        <div className="flex-1 flex flex-col text-left">
+                          <h3 className="font-semibold text-lg">
+                            {user.username}
+                          </h3>
+                          <p className="text-sm text-gray-600 dark:text-gray-300">
+                            {user.name}
+                          </p>
+                        </div>
+                      </button>
+                      {activeSection === user.username && (
+                        <div className="block sm:hidden mt-4">
+                          <ProfileSectionFriendsPage
+                            userDetails={user}
+                            currentUser={currentUser}
+                          />
+                        </div>
+                      )}
+                    </div>
+                  ))
+                ) : (
+                  <p className="text-center text-gray-600 dark:text-gray-300 no-scrollbar">
+                    No users found
+                  </p>
+                )}
+              </div>
             </div>
-            <div className="w-full md:w-5/6 overflow-auto no-scrollbar rounded-lg flex flex-col text-black dark:text-white text-xl">
+            <div className="w-full md:w-4/5 overflow-auto no-scrollbar  rounded-lg flex flex-col text-black dark:text-white text-xl">
               {activeSection ? (
-                <div className="hidden md:block w-full overflow-auto no-scrollbar">
+                <div className="w-full overflow-auto no-scrollbar">
                   <ProfileSectionFriendsPage
                     userDetails={users.find(
                       user => user.username === activeSection
