bc568aae7d1d87838bf2b5f73f5c9a6799b02269
Rummyspace <abilashnarayanan2001@gmail.com>
2024-06-11 10:15:14 +0530

    Explore page completed

--- frontend/src/Components/Explore/ExplorePageComponent.tsx
+++ frontend/src/Components/Explore/ExplorePageComponent.tsx
@@ -2,14 +2,20 @@ import React, { useEffect, useState } from "react";
 import Layout from "../Layout";
 import { getAllPublicPostsForExplore } from "../../API/Post/post";
 import { useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 const MediaItem = ({ type, src }) => {
   if (type === "image") {
     return <img src={src} alt="Media" className="w-full h-full object-cover" />;
   } else if (type === "video") {
     return (
-      <video autoPlay loop controls className="w-full h-full object-cover">
-        <source  src={src} type="video/mp4" />
+      <video
+        autoPlay
+        loop
+        controls
+        muted
+        className="w-full h-full object-cover">
+        <source src={src} type="video/mp4" />
       </video>
     );
   }
@@ -17,12 +23,14 @@ const MediaItem = ({ type, src }) => {
 };
 
 const getRandomSpan = () => {
-  const spans = [5, 4, 5, 6, 1, 3, 4, 2, 4];
+  const spans = [1, 2, 3, 4, 5];
   return spans[Math.floor(Math.random() * spans.length)];
 };
 
 const ExplorePageComponent = () => {
   const [allPosts, setAllPosts] = useState([]);
+  const [searchContent, setSearchedContent] = useState("");
+  const [activeHashtag, setActiveHashtag] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -38,12 +46,73 @@ const ExplorePageComponent = () => {
     fetchAllPosts();
   }, []);
 
+  const filteredPosts = allPosts.filter(post => {
+    const lowercasedSearchContent = searchContent.toLowerCase();
+    const lowercasedActiveHashtag = activeHashtag.toLowerCase();
+    const username = post.username?.toLowerCase() ?? "";
+    const name = post.name?.toLowerCase() ?? "";
+    const captions = post.captions?.toLowerCase() ?? "";
+    const location = post.location?.toLowerCase() ?? "";
+    const hashtags = post.hashtags?.map(hashtag => hashtag.toLowerCase()) ?? [];
+
+    return (
+      username.includes(lowercasedSearchContent) ||
+      name.includes(lowercasedSearchContent) ||
+      captions.includes(lowercasedSearchContent) ||
+      location.includes(lowercasedSearchContent) ||
+      (lowercasedActiveHashtag &&
+        hashtags.some(hashtag => hashtag.includes(lowercasedActiveHashtag)))
+    );
+  });
+
+  const allAvailableHashtags = allPosts.reduce((acc, post) => {
+    if (post.hashtags) {
+      const lowercasedHashtags = post.hashtags.map(hashtag =>
+        hashtag.toLowerCase()
+      );
+      return acc.concat(lowercasedHashtags);
+    }
+    return acc;
+  }, []);
+
   return (
     <Layout>
-      <main className="bg-gray-800 min-h-screen dark:bg-gray-700 text-black dark:text-white p-2 w-full sm:-ml-2">
+      <main className="bg-gray-800 min-h-screen dark:bg-gray-700 text-black dark:text-white gap-2 p-2 w-full sm:-ml-2 overflow-y-auto no-scrollbar">
+        <div className="flex flex-col gap-2">
+          <div className="p-2 rounded-lg w-full bg-gray-200 dark:bg-gray-800 flex items-center">
+                
+                <FaSearch onClick={() => { setSearchedContent(''); setActiveHashtag(""); }} className="w-8 h-8 bg-gray-200 dark:bg-gray-800 p-2 rounded-lg" />
+            <input
+              type="text"
+              onChange={e => {
+                setSearchedContent(e.target.value);
+                setActiveHashtag("");
+              }}
+              placeholder="Search username, hashtags, locations, captions.."
+              className="bg-gray-200 dark:bg-gray-800 text-white w-full p-2 rounded-lg mr-2 focus:outline-none"
+            />
+          </div>
+          <div className="p-2 rounded-lg w-full bg-slate-200 dark:bg-slate-900   flex-nowrap gap-2 mb-2 overflow-x-auto no-scrollbar flex">
+            {allAvailableHashtags.map((hashtag, index) => (
+              <button
+                key={index}
+                onClick={() => {
+                  setActiveHashtag(hashtag);
+                  setSearchedContent(hashtag);
+                }}
+                className={`p-2 rounded-lg ${
+                  activeHashtag === hashtag
+                    ? "bg-slate-300 dark:bg-slate-700  text-black dark:text-white"
+                    : "bg-slate-100 dark:bg-slate-800  text-black dark:text-white"
+                }`}>
+                {hashtag}
+              </button>
+            ))}
+          </div>
+        </div>
         <div className="h-full w-full overflow-y-auto no-scrollbar p-2 bg-gray-200 dark:bg-gray-900 mb-2 md:mb-4 lg:mb-8">
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2">
-            {allPosts.map((post, index) => (
+            {filteredPosts.map((post, index) => (
               <React.Fragment key={post._id + index}>
                 {post.images && (
                   <div
