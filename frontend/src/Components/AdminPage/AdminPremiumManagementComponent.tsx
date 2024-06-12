6b88897e04dcd2b8f50e43db7e5ea19c07a806c4
Rummyspace <abilashnarayanan2001@gmail.com>
2024-06-12 20:28:52 +0530

    admin

--- frontend/src/Components/AdminPage/AdminPremiumManagementComponent.tsx
+++ frontend/src/Components/AdminPage/AdminPremiumManagementComponent.tsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { fetchAllPremiumAccountData } from "../../API/Admin/admin";
+import {
+  fetchAllPremiumAccountData,
+  getAllUsersForAdmin,
+} from "../../API/Admin/admin";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ReactPaginate from "react-paginate";
-import Swal from "sweetalert2";
 import { PremiumAccountInterface } from "../../Types/userProfile";
 
 const AdminPremiumManagementComponent: React.FC = () => {
-  const [premiumAccounts, setPremiumAccounts] = useState<PremiumAccountInterface[]>([]);
+  const [premiumAccounts, setPremiumAccounts] = useState<
+    PremiumAccountInterface[]
+  >([]);
+  const [users, setUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [accountsPerPage] = useState(5);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [searchUserId, setSearchUserId] = useState("");
+  const [foundUser, setFoundUser] = useState(null);
 
   const getPremiumAccountData = async () => {
     try {
       const response = await fetchAllPremiumAccountData();
-      console.log("resp: ", response.premiumAccountRequests)
       if (response) {
         setPremiumAccounts(response.premiumAccountRequests);
       } else {
@@ -26,6 +32,21 @@ const AdminPremiumManagementComponent: React.FC = () => {
       toast.error("An error occurred while fetching premium account data");
     }
   };
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const allUsers = await getAllUsersForAdmin();
+        setUsers(allUsers.users);
+        console.log(allUsers.users);
+      } catch (error) {
+        toast.error("Failed to load users");
+        console.error("Failed to fetch users", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
 
   useEffect(() => {
     getPremiumAccountData();
@@ -43,12 +64,32 @@ const AdminPremiumManagementComponent: React.FC = () => {
     setSelectedImage(null);
   };
 
-  const ImageModal: React.FC<{ imageUrl: string; onClose: () => void }> = ({ imageUrl, onClose }) => {
+  const handleSearch = () => {
+    const user = users.find(
+      user =>
+        user._id === searchUserId ||
+        user.username === searchUserId ||
+        user.name === searchUserId
+    );
+    if (user) {
+      setFoundUser(user);
+    } else {
+      toast.error("User not found");
+      setFoundUser(null);
+    }
+  };
+
+  const ImageModal: React.FC<{ imageUrl: string; onClose: () => void }> = ({
+    imageUrl,
+    onClose,
+  }) => {
     return (
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50">
         <div className="bg-white p-4 rounded-lg max-w-4xl w-full">
           <img src={imageUrl} alt="Large Image" className="max-w-full h-auto" />
-          <button onClick={onClose} className="block mt-4 mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button
+            onClick={onClose}
+            className="block mt-4 mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Close
           </button>
         </div>
@@ -57,54 +98,179 @@ const AdminPremiumManagementComponent: React.FC = () => {
   };
 
   const offset = currentPage * accountsPerPage;
-  const currentAccounts = premiumAccounts.slice(offset, offset + accountsPerPage);
+  const currentAccounts = premiumAccounts.slice(
+    offset,
+    offset + accountsPerPage
+  );
   const pageCount = Math.ceil(premiumAccounts.length / accountsPerPage);
 
   return (
     <main className="flex-1 pt-2 p-2 overflow-auto bg-gray-800 dark:bg-gray-700 text-black dark:text-white">
       <div className="p-4 rounded-lg bg-gray-100 dark:bg-gray-900 text-black dark:text-white h-full overflow-y-auto no-scrollbar flex flex-col">
-        <h1 className="text-center text-xl font-semibold mb-4">Premium Account Management</h1>
+        <h1 className="text-center text-xl font-semibold mb-4">
+          Premium Account Management
+        </h1>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Search User"
+            value={searchUserId}
+            onChange={e => setSearchUserId(e.target.value)}
+            className="p-2 rounded bg-gray-200 dark:bg-gray-700 text-black dark:text-white"
+          />
+          <button
+            onClick={handleSearch}
+            className="ml-2 px-4 py-2 rounded bg-blue-500 hover:bg-blue-700 text-white">
+            Search
+          </button>
+        </div>
+        {foundUser && (
+          <div className="mb-4 p-4 rounded bg-gray-200 dark:bg-gray-700 text-black dark:text-white">
+            <h2 className="text-lg font-semibold mb-2">User Details</h2>
+            <p>
+              <strong>Name:</strong> {foundUser.name}
+            </p>
+            <p>
+              <strong>Username:</strong> {foundUser.username}
+            </p>
+            <p>
+              <strong>Email:</strong> {foundUser.email}
+            </p>
+            <p>
+              <strong>Bio:</strong> {foundUser.bio || "N/A"}
+            </p>
+            <p>
+              <strong>Private:</strong> {foundUser.isPrivate ? "Yes" : "No"}
+            </p>
+            <p>
+              <strong>Suspended:</strong> {foundUser.isSuspended ? "Yes" : "No"}
+            </p>
+            <p>
+              <strong>Google Login:</strong>{" "}
+              {foundUser.isGoogleSignedIn ? "Yes" : "No"}
+            </p>
+            <p>
+              <strong>Blocked:</strong> {foundUser.isBlocked ? "Yes" : "No"}
+            </p>
+            <img
+              src={foundUser.dp}
+              alt={`${foundUser.name}'s profile`}
+              className="w-32 h-32 rounded-full mt-4"
+            />
+          </div>
+        )}
         <div className="overflow-x-auto">
           <table className="min-w-full bg-white dark:bg-gray-800">
             <thead>
               <tr>
-                <th className="px-4 py-2">User ID</th>
+                <th className="px-4 py-2">User</th>
                 <th className="px-4 py-2">Is Premium</th>
                 <th className="px-4 py-2">Premium Expires At</th>
                 <th className="px-4 py-2">Payment Details</th>
-                <th className="px-4 py-2">Is Requested</th>
                 <th className="px-4 py-2">Is Admin Approved</th>
                 <th className="px-4 py-2">Documents</th>
               </tr>
             </thead>
             <tbody>
-              {currentAccounts.map((account) => (
-                <tr key={account._id} className="bg-gray-100 dark:bg-gray-700">
-                  <td className="border px-4 py-2">{account.userId}</td>
-                  <td className="border px-4 py-2">{account.isPremium ? 'Yes' : 'No'}</td>
-                  <td className="border px-4 py-2">{account.premiumExpiresAt ? new Date(account.premiumExpiresAt).toLocaleDateString() : 'N/A'}</td>
-                  <td className="border px-4 py-2">{account.paymentDetails || 'N/A'}</td>
-                  <td className="border px-4 py-2">{account.premiumRequest.isRequested ? 'Yes' : 'No'}</td>
-                  <td className="border px-4 py-2">{account.premiumRequest.isAdminApproved ? 'Yes' : 'No'}</td>
-                  <td className="border px-4 py-2">
-                    {account.premiumRequest.documents.length > 0 ? (
-                      account.premiumRequest.documents.map((doc, index) => (
-                        <div key={index} className="mb-2">
-                          <p><strong>Type:</strong> {doc.type}</p>
-                          {doc.image.map((img, imgIndex) => (
-                            <img key={imgIndex} src={img} alt={`Document ${index + 1}`} className="w-32 h-32 object-cover m-1 border border-gray-300 rounded cursor-pointer" onClick={() => openModal(img)} />
-                          ))}
+              {currentAccounts.map(account => {
+                const user = users.find(user => user._id === account.userId);
+                return (
+                  <tr
+                    key={account._id}
+                    className="bg-gray-100 dark:bg-gray-700">
+                    <td className="border px-4 py-2">
+                      {user ? (
+                        <div>
+                          <p>
+                            <strong>UserId:</strong> {user._id}
+                          </p>
+                          <p>
+                            <strong>Name:</strong> {user.name}
+                          </p>
+                          <p>
+                            <strong>Username:</strong> {user.username}
+                          </p>
+                          <p>
+                            <strong>Email:</strong> {user.email}
+                          </p>
                         </div>
-                      ))
-                    ) : (
-                      'No documents'
-                    )}
-                  </td>
-                </tr>
-              ))}
+                      ) : (
+                        "User details not found"
+                      )}
+                    </td>
+                    <td className="border px-4 py-2">
+                      {account.isPremium ? (
+                        <div className="bg-slate-200 dark:bg-slate-800 p-2 rounded-md text-center gap-2 flex flex-col">
+                          <p className="bg-green-500">Yes</p>
+                        </div>
+                      ) : (
+                        <div className="bg-slate-200 dark:bg-slate-800 p-2 rounded-md text-center gap-2 flex flex-col">
+                          <p className="bg-red-500">No</p>
+                        </div>
+                      )}
+                    </td>
+                    <td className="border px-4 py-2">
+                      {account.premiumExpiresAt
+                        ? new Date(
+                            account.premiumExpiresAt
+                          ).toLocaleDateString()
+                        : "N/A"}
+                    </td>
+                    <td className="border px-4 py-2">
+                      <p>Payment Id:</p>
+                      <div className="bg-slate-200 dark:bg-slate-800 p-2 rounded-md text-center gap-2 flex flex-col">
+                        {account.paymentDetails || "N/A"}
+                      </div>
+                      <p>
+                        Created Date:{" "}
+                        {new Date(account.createdAt).toLocaleString()}
+                      </p>
+                      <p>
+                        Last Updated Date:{"  "}{" "}
+                        {new Date(account.updatedAt).toLocaleString()}
+                      </p>
+                    </td>
+                    <td className="border px-4 py-2">
+                      {account.premiumRequest.isAdminApproved ? (
+                        <div className="bg-slate-200 dark:bg-slate-800 p-2 rounded-md text-center gap-2 flex flex-col">
+                          Yes
+                          <button className="bg-red-500">Remove Premium</button>
+                        </div>
+                      ) : (
+                        <div className="bg-slate-200 dark:bg-slate-800 p-2 rounded-md text-center gap-2 flex flex-col">
+                          No
+                          <button className="bg-green-500">
+                            Approve Premium
+                          </button>
+                        </div>
+                      )}
+                    </td>
+                    <td className="border px-4 py-2">
+                      {account.premiumRequest.documents.length > 0
+                        ? account.premiumRequest.documents.map((doc, index) => (
+                            <div key={index} className="mb-2">
+                              <p>
+                                <strong>Type:</strong> {doc.type}
+                              </p>
+                              {doc.image.map((img, imgIndex) => (
+                                <img
+                                  key={imgIndex}
+                                  src={img}
+                                  alt={`Document ${index + 1}`}
+                                  className="w-32 h-32 object-cover m-1 border border-gray-300 rounded cursor-pointer"
+                                  onClick={() => openModal(img)}
+                                />
+                              ))}
+                            </div>
+                          ))
+                        : "No documents"}
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
-          
+
           <ReactPaginate
             previousLabel={"Previous"}
             nextLabel={"Next"}
@@ -125,10 +291,12 @@ const AdminPremiumManagementComponent: React.FC = () => {
           />
         </div>
       </div>
-      
-      {selectedImage && <ImageModal imageUrl={selectedImage} onClose={closeModal} />}
+
+      {selectedImage && (
+        <ImageModal imageUrl={selectedImage} onClose={closeModal} />
+      )}
     </main>
   );
-}
+};
 
 export default AdminPremiumManagementComponent;
