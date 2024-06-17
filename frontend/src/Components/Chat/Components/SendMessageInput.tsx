502303b0de03a42635ff0c88a462380a8e4cb6bf
Rummyspace <abilashnarayanan2001@gmail.com>
2024-06-17 23:02:32 +0530

    before adding multiple fields

--- frontend/src/Components/Chat/Components/SendMessageInput.tsx
+++ frontend/src/Components/Chat/Components/SendMessageInput.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
-import { FaSmile } from "react-icons/fa";
+import { FaPaperPlane, FaSmile } from "react-icons/fa";
 import useSendMessages from "../../../Hooks/useSendMessages";
 import useConversation from "../../../Hooks/useConversations";
 import useUserDetails from "../../../Hooks/useUserDetails";
+import { FiPaperclip } from "react-icons/fi";
 
 const SendMessageInput: React.FC = () => {
   const [message, setMessage] = useState("");
   const { loading, sendMessage } = useSendMessages();
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [fieldsToChoose, setFieldsToChoose] = useState(false);
   const { selectedConversation } = useConversation();
   const currentUser = useUserDetails();
 
@@ -39,26 +41,35 @@ const SendMessageInput: React.FC = () => {
       {canChat && (
         <>
           <form className="flex items-center w-full" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Type your message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              className="flex-grow p-2 mr-2 rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
-            />
-            <button
+          <button
               type="button"
               className="p-2 bg-gray-300 dark:bg-gray-700 text-yellow-500 rounded-lg mr-2"
               onClick={() => setShowEmojiPicker((prev) => !prev)}
             >
               <FaSmile />
             </button>
+            <input
+              type="text"
+              placeholder="Type your message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="flex-grow p-2 mr-2 rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+            
+            />
+           <button
+              type="button"
+              className="p-2 bg-gray-300 dark:bg-gray-700   rounded-lg mr-2"
+              onClick={() => setFieldsToChoose(true)}
+            >
+           <FiPaperclip />
+               
+            </button>
             <button
               type="submit"
               className="p-2 bg-blue-500 text-white rounded-lg"
               disabled={loading}
             >
-              {loading ? <div className="loading loading-spinner"></div> : "Send"}
+              {loading ? <div className="loading loading-spinner"></div> : <FaPaperPlane/>}
             </button>
           </form>
           {showEmojiPicker && (
