6d45d6243a7830cde2dfd0623044c9c4500a8eae
Rummyspace <abilashnarayanan2001@gmail.com>
2024-06-21 15:42:52 +0530

    UI

--- frontend/src/Components/Chat/Components/ChatIndividualTopPortion.tsx
+++ frontend/src/Components/Chat/Components/ChatIndividualTopPortion.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import useConversation from "../../../Hooks/useConversations";
 import { useSocketContext } from "../../../Context/SocketContext";
+import { IoIosCall } from "react-icons/io";
+import { RxDotsVertical } from "react-icons/rx";
+import { MdVideoCall } from "react-icons/md";
 
 const ChatIndividualTopPortion = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
@@ -22,24 +25,32 @@ const ChatIndividualTopPortion = () => {
   const isOnline = onlineUsers.includes(selectedConversation._id);
 
   return (
-    <div className="h-16 mb-5 -m-3 bg-gray-300 dark:bg-gray-900 rounded-t-lg shadow-sm flex items-center p-4">
+    <div className="h-16 bg-gray-300 dark:bg-gray-700 rounded-lg shadow-sm flex items-center p-4">
       <FaArrowLeft
-        className="text-xl cursor-pointer mr-4 text-gray-900 dark:text-gray-100 md:hidden" // Hide on medium and larger screens
+        className="text-xl cursor-pointer mr-4 text-gray-900 dark:text-gray-100 md:hidden"
         onClick={handleBackClick}
       />
-      <div className="chat-image avatar mr-3">
-        <div className="w-10 rounded-full">
+      <div className="flex-shrink-0 mr-3">
+        <div className="w-10 h-10 rounded-full overflow-hidden">
           <img
             alt={`${selectedConversation.username}'s avatar`}
             src={selectedConversation.dp}
+            className="w-full h-full object-cover"
           />
         </div>
       </div>
-      <div>
+      <div className="flex-1">
         <div className="text-lg font-semibold text-gray-900 dark:text-gray-100">
           {selectedConversation.username}
         </div>
-        <div className="text-sm text-gray-600 dark:text-gray-400">{isOnline ?"Online" : "Offline"}</div>
+        <div className="text-sm text-gray-600 dark:text-gray-400">
+          {isOnline ? "Online" : "Offline"}
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <IoIosCall className="text-2xl text-gray-900 dark:text-gray-100 cursor-pointer" />
+        <MdVideoCall className="text-2xl text-gray-900 dark:text-gray-100 cursor-pointer" />
+        <RxDotsVertical  className="text-2xl text-gray-900 dark:text-gray-100 cursor-pointer" />
       </div>
     </div>
   );
