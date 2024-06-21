988f29d6e2fc55cc84b4080630d903b4f5f33f66
Rummyspace <abilashnarayanan2001@gmail.com>
2024-06-21 17:28:40 +0530

    Added options for individual chat

--- frontend/src/Components/Chat/Components/ChatIndividualTopPortion.tsx
+++ frontend/src/Components/Chat/Components/ChatIndividualTopPortion.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import useConversation from "../../../Hooks/useConversations";
 import { useSocketContext } from "../../../Context/SocketContext";
@@ -8,6 +8,30 @@ import { MdVideoCall } from "react-icons/md";
 
 const ChatIndividualTopPortion = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
+  const [displayOptions, setDisplayOptions] = useState(false);
+  const optionsRef = useRef(null);
+
+  const handleClickOutside = (event: { target: any; }) => {
+    if (optionsRef.current && !optionsRef.current.contains(event.target)) {
+      setDisplayOptions(false);
+    }
+  };
+
+  useEffect(() => {
+    if (displayOptions) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [displayOptions]);
+
+  const toggleDisplayOptions = () => {
+    setDisplayOptions(!displayOptions);
+  };
 
   const handleBackClick = () => {
     setSelectedConversation(null);
@@ -25,7 +49,7 @@ const ChatIndividualTopPortion = () => {
   const isOnline = onlineUsers.includes(selectedConversation._id);
 
   return (
-    <div className="h-16 bg-gray-300 dark:bg-gray-700 rounded-lg shadow-sm flex items-center p-4">
+    <div className="h-16 bg-gray-300 dark:bg-gray-700 rounded-lg shadow-sm flex items-center p-4 relative">
       <FaArrowLeft
         className="text-xl cursor-pointer mr-4 text-gray-900 dark:text-gray-100 md:hidden"
         onClick={handleBackClick}
@@ -50,8 +74,32 @@ const ChatIndividualTopPortion = () => {
       <div className="flex items-center space-x-4">
         <IoIosCall className="text-2xl text-gray-900 dark:text-gray-100 cursor-pointer" />
         <MdVideoCall className="text-2xl text-gray-900 dark:text-gray-100 cursor-pointer" />
-        <RxDotsVertical  className="text-2xl text-gray-900 dark:text-gray-100 cursor-pointer" />
+        <RxDotsVertical
+          onClick={toggleDisplayOptions}
+        
+          className="text-2xl text-gray-900 dark:text-gray-100 cursor-pointer"
+        />
       </div>
+
+      {displayOptions && (
+        <div ref={optionsRef} className="absolute right-2 top-16 z-30 mt-2 w-48 bg-slate-200 dark:bg-slate-700 p-2 rounded-lg shadow-lg">
+          <div className="flex flex-col space-y-2">
+            <div className="cursor-pointer p-2 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-md">
+           Clear Chat
+            </div>
+
+            <div className="cursor-pointer p-2 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-md">
+              Block User
+            </div>
+            <div className="cursor-pointer p-2 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-md">
+             Mute Notifications
+            </div>
+            <div className="cursor-pointer p-2 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-md">
+             Unmute Notifications
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
